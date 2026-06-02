3335bd6d73c5eba2f9e2862d53f7f574615a3fa4
Pengxh <290677893@qq.com>
2026-06-03 00:12:03 +0800

    feat(mainwindow): 添加串口配置和数据读取功能

--- SerialPortObserver.cpp
+++ SerialPortObserver.cpp
@@ -2,6 +2,14 @@
 
 #include <QSerialPort>
 
+#ifdef _WIN32
+#define PortName "COM1"
+#else
+// Linux, macOS 等
+#define PortName "dev/ttyS0"
+#endif
+#define BaudRate 19200
+
 std::unique_ptr<SerialPortObserver> SerialPortObserver::_instance = nullptr;
 
 SerialPortObserver::SerialPortObserver() : _logger("SerialPortObserver") {
@@ -16,10 +24,33 @@ SerialPortObserver *SerialPortObserver::get() {
   return _instance.get();
 }
 
+// 需要改为通过按钮控制串口开关，然后再开始读数
 void SerialPortObserver::run() {
-  while (!isInterruptionRequested()) {
-    emit signalDataReceived(QByteArray("Hello from SerialPortObserver!"));
+  QSerialPort serial;
+  serial.setPortName(PortName);
+  serial.setBaudRate(BaudRate);
+  serial.setDataBits(QSerialPort::Data8);
+  serial.setParity(QSerialPort::NoParity);
+  serial.setStopBits(QSerialPort::OneStop);
+  serial.setFlowControl(QSerialPort::NoFlowControl);
+
+  if (!serial.open(QIODevice::ReadOnly)) {
+    _logger.e("Failed to open serial port");
+    return;
   }
+
+  _logger.dFmt("%s opened successfully", PortName);
+
+  while (!isInterruptionRequested()) {
+    if (serial.waitForReadyRead(100)) {
+      QByteArray data = serial.readAll();
+      if (!data.isEmpty()) {
+        emit signalDataReceived(data);
+      }
+    }
+  }
+  serial.close();
+  _logger.i("Serial port closed");
 }
 
 SerialPortObserver::~SerialPortObserver() {
