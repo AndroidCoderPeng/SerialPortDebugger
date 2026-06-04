af37f922ba683d0b643322e2f9b0eb622a9bf4fb
pengxianhong <290677893@qq.com>
2026-06-04 11:57:37 +0800

    feat(SerialPortObserver): 重构串口观察者，简化串口管理逻辑并改进数据读取方式

--- SerialPortObserver.cpp
+++ SerialPortObserver.cpp
@@ -1,121 +1,152 @@
 #include "SerialPortObserver.hpp"
 
-#ifdef _WIN32
-#define DefaultPort "COM1"
-#else
-#define DefaultPort "/dev/ttyS0"
-#endif
-#define DefaultBaudRate 19200
+#include <QSerialPort>
 
-std::unique_ptr<SerialPortObserver> SerialPortObserver::_instance = nullptr;
+#ifdef _WIN32
+#define PortName "COM1"
+#else
+#define PortName "/dev/ttyS0"
+#endif
+#define BaudRate 19200
 
 SerialPortObserver::SerialPortObserver()
-    : _logger("SerialPortObserver"), _portName(DefaultPort),
-      _baudRate(DefaultBaudRate) {
+    : QThread(), _logger("SerialPortObserver") {
+  this->start();
   _logger.i("initialized succeed.");
 }
 
-SerialPortObserver *SerialPortObserver::get() {
-  if (!_instance) {
-    _instance = std::unique_ptr<SerialPortObserver>(new SerialPortObserver());
-    _instance->start();
-  }
-  return _instance.get();
-}
+// bool SerialPortObserver::open(const QString &portName, qint32 baudRate,
+//                               const QString &dataBits, const QString &parity,
+//                               const QString &stopBits,
+//                               const QString &flowControl) {
+//   _portName = portName;
+//   _baudRate = baudRate;
 
-bool SerialPortObserver::open(const QString &portName, qint32 baudRate,
-                              const QString &dataBits, const QString &parity,
-                              const QString &stopBits,
-                              const QString &flowControl) {
-  _portName = portName;
-  _baudRate = baudRate;
+//   if (portPtr && portPtr->isOpen()) {
+//     portPtr->close();
+//   }
 
-  if (portPtr && portPtr->isOpen()) {
-    portPtr->close();
-  }
+//   portPtr = new QSerialPort(this);
+//   portPtr->setPortName(_portName);
+//   portPtr->setBaudRate(_baudRate);
 
-  portPtr = new QSerialPort(this);
-  portPtr->setPortName(_portName);
-  portPtr->setBaudRate(_baudRate);
+//   // 设置数据位
+//   if (dataBits == "5") {
+//     portPtr->setDataBits(QSerialPort::Data5);
+//   } else if (dataBits == "6") {
+//     portPtr->setDataBits(QSerialPort::Data6);
+//   } else if (dataBits == "7") {
+//     portPtr->setDataBits(QSerialPort::Data7);
+//   } else {
+//     portPtr->setDataBits(QSerialPort::Data8); // 默认 8
+//   }
 
-  // 设置数据位
-  if (dataBits == "5") {
-    portPtr->setDataBits(QSerialPort::Data5);
-  } else if (dataBits == "6") {
-    portPtr->setDataBits(QSerialPort::Data6);
-  } else if (dataBits == "7") {
-    portPtr->setDataBits(QSerialPort::Data7);
-  } else {
-    portPtr->setDataBits(QSerialPort::Data8); // 默认 8
-  }
+//   // 设置校验位
+//   if (parity == "Even") {
+//     portPtr->setParity(QSerialPort::EvenParity);
+//   } else if (parity == "Odd") {
+//     portPtr->setParity(QSerialPort::OddParity);
+//   } else {
+//     portPtr->setParity(QSerialPort::NoParity); // 默认无
+//   }
 
-  // 设置校验位
-  if (parity == "Even") {
-    portPtr->setParity(QSerialPort::EvenParity);
-  } else if (parity == "Odd") {
-    portPtr->setParity(QSerialPort::OddParity);
-  } else {
-    portPtr->setParity(QSerialPort::NoParity); // 默认无
-  }
+//   // 设置停止位
+//   if (stopBits == "2") {
+//     portPtr->setStopBits(QSerialPort::TwoStop);
+//   } else {
+//     portPtr->setStopBits(QSerialPort::OneStop); // 默认 1
+//   }
 
-  // 设置停止位
-  if (stopBits == "2") {
-    portPtr->setStopBits(QSerialPort::TwoStop);
-  } else {
-    portPtr->setStopBits(QSerialPort::OneStop); // 默认 1
-  }
+//   // 设置流控
+//   if (flowControl == "Hardware") {
+//     portPtr->setFlowControl(QSerialPort::HardwareControl);
+//   } else if (flowControl == "Software") {
+//     portPtr->setFlowControl(QSerialPort::SoftwareControl);
+//   } else {
+//     portPtr->setFlowControl(QSerialPort::NoFlowControl); // 默认无
+//   }
 
-  // 设置流控
-  if (flowControl == "Hardware") {
-    portPtr->setFlowControl(QSerialPort::HardwareControl);
-  } else if (flowControl == "Software") {
-    portPtr->setFlowControl(QSerialPort::SoftwareControl);
-  } else {
-    portPtr->setFlowControl(QSerialPort::NoFlowControl); // 默认无
-  }
+//   if (!portPtr->open(QIODevice::ReadWrite)) {
+//     _logger.eFmt("Failed to open %s", _portName.toStdString().c_str());
+//     return false;
+//   }
 
-  if (!portPtr->open(QIODevice::ReadWrite)) {
-    _logger.eFmt("Failed to open %s", _portName.toStdString().c_str());
-    return false;
-  }
+//   _logger.dFmt("%s opened successfully", _portName.toStdString().c_str());
+//   return true;
+// }
 
-  _logger.dFmt("%s opened successfully", _portName.toStdString().c_str());
-  return true;
-}
+// void SerialPortObserver::write(const QByteArray data) {
+//   if (portPtr && portPtr->isOpen()) {
+//     portPtr->write(data);
+//   }
+// }
 
-void SerialPortObserver::write(const QByteArray data) {
-  if (portPtr && portPtr->isOpen()) {
-    portPtr->write(data);
-  }
-}
+// void SerialPortObserver::close() {
+//   if (portPtr && portPtr->isOpen()) {
+//     portPtr->close();
+//     _logger.dFmt("%s closed", _portName.toStdString().c_str());
+//   }
+// }
 
-void SerialPortObserver::close() {
-  if (portPtr && portPtr->isOpen()) {
-    portPtr->close();
-    _logger.dFmt("%s closed", _portName.toStdString().c_str());
-  }
-}
-
-bool SerialPortObserver::isOpen() const { return portPtr && portPtr->isOpen(); }
+// bool SerialPortObserver::isOpen() const { return portPtr &&
+// portPtr->isOpen(); }
 
 // 需要改为通过按钮控制串口开关，然后再开始读数
 void SerialPortObserver::run() {
+  QSerialPort serialPort;
+  serialPort.setPortName(PortName);
+  serialPort.setBaudRate(BaudRate);
+  serialPort.setDataBits(QSerialPort::Data8);
+  serialPort.setParity(QSerialPort::NoParity);
+  serialPort.setStopBits(QSerialPort::OneStop);
+  serialPort.setFlowControl(QSerialPort::NoFlowControl);
+
   while (!isInterruptionRequested()) {
-    if (portPtr && portPtr->isOpen()) {
-      if (portPtr->waitForReadyRead(100)) {
-        QByteArray data = portPtr->readAll();
-        if (!data.isEmpty()) {
-          emit signalDataReceived(data);
-        }
+    if (!serialPort.isOpen()) {
+      if (!serialPort.open(QIODevice::ReadOnly)) {
+        _logger.eFmt("Failed to open %s: %s", PortName,
+                     serialPort.errorString().toStdString().c_str());
+        msleep(1000);
+        continue;
       }
-    } else {
-      QThread::msleep(100);
+      _logger.dFmt("%s opened successfully, baud %d", PortName, BaudRate);
     }
+
+    if (serialPort.waitForReadyRead(200)) {
+      QByteArray data = serialPort.readAll();
+      while (serialPort.waitForReadyRead(10)) {
+        data += serialPort.readAll();
+      }
+
+      if (!data.isEmpty()) {
+        emit signalDataReceived(data);
+      }
+      continue;
+    }
+
+    if (isInterruptionRequested()) {
+      break;
+    }
+
+    const QSerialPort::SerialPortError err = serialPort.error();
+    if (err == QSerialPort::NoError || err == QSerialPort::TimeoutError) {
+      continue;
+    }
+
+    _logger.eFmt("Serial error: %s",
+                 serialPort.errorString().toStdString().c_str());
+
+    serialPort.close();
+    msleep(300);
+  }
+
+  if (serialPort.isOpen()) {
+    serialPort.close();
+    _logger.dFmt("%s closed", PortName);
   }
 }
 
 SerialPortObserver::~SerialPortObserver() {
-  close();
-  quit();
+  requestInterruption();
   wait();
 }
--- SerialPortObserver.hpp
+++ SerialPortObserver.hpp
@@ -1,37 +1,28 @@
 #ifndef SERIALPORTOBSERVER_HPP
 #define SERIALPORTOBSERVER_HPP
 
-#include <QSerialPort>
 #include <QThread>
 
 #include "Logger.hpp"
 
+// 如果串口的生命期和应用本身保持一致，只读取不写入，不涉及跨线程操作，那么直接继承QThread就是合理的
 class SerialPortObserver : public QThread {
   Q_OBJECT
 
 public:
-  static SerialPortObserver *get();
+  SerialPortObserver(QObject *parent = nullptr);
   ~SerialPortObserver();
 
-  bool open(const QString &portName, qint32 baudRate,
-            const QString &dataBits = "", const QString &parity = "",
-            const QString &stopBits = "", const QString &flowControl = "");
-  void write(const QByteArray data);
-  void close();
-  bool isOpen() const;
+  static inline SerialPortObserver *get() {
+    static SerialPortObserver instance;
+    return &instance;
+  }
 
 signals:
   void signalDataReceived(const QByteArray &data);
 
 private:
-  SerialPortObserver();
-
-  static std::unique_ptr<SerialPortObserver> _instance;
-
   Logger _logger;
-  QSerialPort *portPtr{nullptr};
-  QString _portName;
-  qint32 _baudRate;
 
   void run() override;
 };
