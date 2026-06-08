cc58860fbd8a1f16d5b0aa33aeebc33f0110b704
pengxianhong <290677893@qq.com>
2026-06-08 09:48:48 +0800

    feat(main): 添加高DPI缩放因子处理，提升应用在高分辨率屏幕上的显示效果

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "DebuggerApplication.hpp"
 
 int main(int argc, char *argv[]) {
+  QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
+      Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
   DebuggerApplication app(argc, argv);
